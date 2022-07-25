1fe08e9009e179ac02e6e4c53cc42eb0c6b6b32a
Jack <tranmjack@gmail.com>
2022-07-25 09:36:23 -0700

    addlikes

--- react-app/src/store/likes.js
+++ react-app/src/store/likes.js
@@ -1,9 +1,17 @@
 const GET_LIKES = 'posts/GET_LIKES'
+const ADD_LIKES = 'posts/ADD_LIKES'
+const EDIT_LIKES = 'posts/EDIT_LIKES'
 
 const getAllLikes = likes => ({
     type: GET_LIKES,
     likes
  });
+
+ const addLikes = like => {
+    type: ADD_LIKES,
+    like
+ }
+
 
 export const getAllLikesThunk = (id) => async(dispatch) => {
     const res = await fetch(`/api/posts/${id}`)
@@ -14,11 +22,26 @@ export const getAllLikesThunk = (id) => async(dispatch) => {
     }
 }
 
+export const addLikesThunk = newLikes => async(dispatch) => {
+    const res = await fetch (`/api/posts`, {
+        method: 'POST',
+        headers: { 'Content-Type' : 'application/json' },
+        body: JSON.stringify(newLike)
+    })
+
+    if (res.ok) {
+        const newLike = await res.json()
+        dispatch(addLikes(newLikes))
+        return newLikes
+    }
+}
+
+
 const initialState = {};
 
 
 export const likesReducer = (state = initialState, action) => {
-    let newState = { ...state }
+    // let newState = { ...state }
     switch (action.type) {
         case GET_LIKES:
             const anotherState = {};
@@ -26,6 +49,9 @@ export const likesReducer = (state = initialState, action) => {
             return anotherState[like.id] = like;
         });
             return anotherState;
+
+        case ADD_LIKES:
+            return { ...state, [action.like.id]: { ...action.like}}
 
     }
 }
