9255535f546b99ae3b5dd0c040a0ccb6f7304d0f
Matthew <mrdicke22@icloud.com>
2022-07-25 11:26:44 -0500

    Post Create

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -1,9 +1,31 @@
-from flask import Blueprint
-from app.models import Post
+from flask import Blueprint, request
+from app.models import Post, db
+from forms.post_form import PostsForm
 
 post_routes = Blueprint('posts', __name__)
+
+def validation_errors_to_error_messages(validation_errors):
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'{field} : {error}')
+    return errorMessages
 
 @post_routes.route('/')
 def posts():
     posts = Post.query.all()
     return {'posts': [post.to_dict() for post in posts]}
+
+@post_routes.route('/post', methods=['POST'])
+def post_post():
+    form = PostsForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        post = Post(
+            caption=form.data['caption'],
+            imageURL=form.data['imageURL']
+        )
+        db.session.add(post)
+        db.session.commit()
+        return post.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
--- react-app/src/store/posts.js
+++ react-app/src/store/posts.js
@@ -1,19 +1,38 @@
-import {csrfFetch} from './csrf';
-
 const GET_ALL_POSTS = 'posts/GET_ALL_POSTS';
+const CREATE_POST = 'posts/CREATE_POST';
 
 const getAllPosts = (posts) => ({
     type: GET_ALL_POSTS,
     posts,
   });
 
+const createPosts = (createdPost) => ({
+    type: CREATE_POST,
+    createdPost,
+})
+
   const initialState = {};
 
 export const getAllPostsThunk = () => async(dispatch) => {
-    const response = await csrfFetch('/api/posts')
+    const response = await fetch('/api/posts')
     const data = await response.json();
     dispatch(getAllPosts(data))
 }
+
+export const createPostThunk = (createdPost) => async(dispatch) => {
+    const response = await fetch('/api/tests/create', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(createdPost),
+    });
+    if (response.ok) {
+      const createdPost = await response.json();
+      dispatch(createPosts(createdPost));
+      return createdPost;
+    }
+  };
 
 export const postsReducer = (state = initialState, action) => {
     let newState = {...state};
@@ -23,6 +42,14 @@ export const postsReducer = (state = initialState, action) => {
                 return newState[test.id] = test;
             })
             return newState;
+        case CREATE_POST:
+            if (!state[action.createdPost.id]) {
+                newState = {
+                    ...state,
+                    [action.createdPost.id]: action.createdPost,
+                };
+            }
+            return newState
         default:
             return state;
     }
