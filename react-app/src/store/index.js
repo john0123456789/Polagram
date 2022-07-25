416b1fb0322be36688e814ac23177a51f9157fd3
Christian Alcantara <chriscayana@gmail.com>
2022-07-24 23:19:57 -0700

    fixed a missing s

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { postsReducer } from './posts';
-import { commentReducer } from './comment';
+import { commentReducer } from './comments';
 import session from './session'
 
 const rootReducer = combineReducers({
