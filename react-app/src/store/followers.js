752af135ad77a0ba5b0787a48b74d4de95c02425
John <john@DESKTOP-O1S0LR6.localdomain>
2022-07-28 09:26:10 -0700

    working on followers page, added seeds, store, component, api routes

--- react-app/src/store/followers.js
+++ react-app/src/store/followers.js
@@ -1,33 +1,79 @@
-export const GET_FOLLOWERS = 'comment/GET_FOLLOWERS'
+const GET_FOLLOWERS = 'followers/GET_FOLLOWERS'
+const ADD_FOLLOW = 'followers/ADD_FOLLOW'
+const DELETE_FOLLOW = 'followers/DELETE_FOLLOW'
 
-const getAllFollowers = (followers) => {
-    return {
-        type: GET_FOLLOWERS,
-        followers
+const getAllFollowers = (followers) => ({
+    type: GET_FOLLOWERS,
+    followers
+ });
+
+ const addFollow = follow => ({
+    type: ADD_FOLLOW,
+    follow
+ })
+
+ const deleteFollow = removeFollow => ({
+    type: DELETE_FOLLOW,
+    removeFollow
+ })
+
+
+export const getAllFollowersThunk = () => async(dispatch) => {
+    const response = await fetch(`/api/followers/`)
+    const data = await response.json();
+    dispatch(getAllFollowers(data.followers))
+    return data.followers
+}
+
+export const addFollowThunk = (id) => async(dispatch) => {
+    const res = await fetch (`/api/followers/${id}`, {
+        method: 'POST',
+        headers: { 'Content-Type' : 'application/json' },
+    })
+
+    if (res.ok) {
+        const newFollow = await res.json()
+        dispatch(addFollow(newFollow))
+        return newFollow
     }
 }
 
 
-export const thunkGetAllFollowers = () => async(dispatch) => {
-    const response = await fetch('/api/followers/')
-    const data = await response.json();
-    dispatch(getAllFollowers(data))
+export const deleteFollowerThunk = (removeFollow, id) => async(dispatch) => {
+    const res = await fetch(`/api/followers/${id}`, {
+        method: 'DELETE',
+    });
+
+    if (res.ok) {
+        const deleted = await res.json(removeFollow);
+        dispatch(deleteFollow(deleted));
+        return deleted
+    }
 }
+
 
 
 const initialState = {};
-const followersReducer = (state = initialState, action) => {
-    let newState = {...state}
+
+export const followersReducer = (state = initialState, action) => {
+    let newState = {...state};
     switch(action.type) {
         case GET_FOLLOWERS:
-            action.followers.forEach(follower => {
-                newState[follower.id] = follower
+            const anotherState = {};
+            action.likes.forEach((follower) => {
+                return anotherState[follower.id] = follower;
             })
+            return anotherState;
+
+        case ADD_FOLLOW:
+            return { ...state, [action.follower.id]: { ...action.follower}}
+
+        case DELETE_FOLLOW:
+            delete newState[action.deleteFollow.id];
             return newState
-        default:
-            return state;
+
+    default:
+        return state;
 
     }
 }
-
-export default followersReducer
