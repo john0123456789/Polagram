b04267114c6c55c3158b191b598d88fb60c16f48
John <john@DESKTOP-O1S0LR6.localdomain>
2022-07-29 14:36:40 -0700

    added splash page with github links, still needs styling

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,9 +1,39 @@
 import './HomePage.css'
+import { AiFillGithub } from 'react-icons/ai'
 
 function HomePage() {
     return (
         <>
-        <h1>Welcome to Polagram!</h1>
+        <h1 className="home-title">Welcome to Polagram!</h1>
+            {/* <a className="github" href="https://github.com/john0123456789" size="25px">
+                <AiFillGithub />
+            </a>
+            <a className="github" href="https://github.com/MonkeyToji" size="25px">
+                <AiFillGithub />
+            </a>
+            <a className="github" href="https://github.com/jackmtran" size="25px">
+                <AiFillGithub />
+            </a>
+            <a className="github" href="https://github.com/Christian-AC" size="25px">
+                <AiFillGithub />
+            </a> */}
+            <div className="github">
+                <a href="https://github.com/john0123456789">
+                    John
+                </a>
+                <a href="https://github.com/MonkeyToji">
+                    Matty
+                </a>
+                <a href="https://github.com/jackmtran">
+                    Jack
+                </a>
+                <a href="https://github.com/Christian-AC">
+                    Christian
+                </a>
+            </div>
+            <ul className="home-scene">
+                <img className="home-img" src="https://i.imgur.com/iSvJUEL.jpg"/>
+            </ul>
         </>
     )
 }
